671c35e4c0b1ce3a959c1a24119d58d44c18b28a
HaiDang Vo <vohaidang0804@gmail.com>
2018-07-27 02:22:26 +0700

    complete sparks

--- examples/canvas2d/sparks/index.js
+++ examples/canvas2d/sparks/index.js
@@ -1,36 +1,56 @@
 //
 var canvas = document.getElementById('canvas') || document.createElement('canvas');
 var ctx = canvas.getContext('2d');
-var pointer = { x: 0, y: 0, hold: false };
+var pointer = { x: window.innerWidth * 0.5, y: window.innerHeight, hold: true };
 var particles = [];
 var config = {
-	gearColor: '#696969',
+	clearColor: '#000000',
+	gearColor: '#333333',
 	gearRadius: 150,
-	sparkColor: '#ffdd00',
-	sparkWidth: 3,
+	sparkColor: ['rgba(225, 242, 24, 0.9)', 'rgba(245, 186, 48, 0.6)', 'rgba(255, 112, 64, 0.1)', 'rgba(255, 96, 96, 0)'],
+	sparkWidth: 6,
 	maxParticles: 300,
+	perParticles: 4,
 	hz: 1000 / 30,
+	maxHz: 1000 / 30,
+	minHz: 1000 / 5,
+	isSlowMotion: false,
+	isExposure: false,
 	speed: {
 		current: 0,
-		increment: 0.002,
+		increment: 0.001,
 		max: Math.PI * 0.12
 	}
 }
 var gears = [];
 var delta = new Date() - 0;
+var buffer = document.createElement('canvas');
 //
-window.onload = function() {
+window.onload = function () {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	init();
 
 	function init() {
-		ctx.fillStyle = '#000';
+		ctx.fillStyle = config.clearColor;
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		//
+		var temp = buffer.getContext('2d');
+		buffer.width = 128;
+		buffer.height = 128;
+		var ct = buffer.width * 0.5;
+		var gradient = temp.createRadialGradient(ct, ct, 0, ct, ct, ct);
+		gradient.addColorStop(0.12, config.sparkColor[0]);
+		gradient.addColorStop(0.45, config.sparkColor[1]);
+		gradient.addColorStop(0.82, config.sparkColor[2]);
+		gradient.addColorStop(0.99, config.sparkColor[3]);
+		temp.fillStyle = gradient;
+		temp.arc(ct, ct, ct, 0, Math.PI * 2);
+		temp.fill();
 		//
 		[0, 1, 2, 3, 4, 5].forEach(i => {
 			gears.push({
-				rotation: 0 + (i + 1) * 1 / 6 * Math.PI,
+				rotation: 0 + (i + 1) * 1 / 9 * Math.PI,
 				radius: config.gearRadius,
 				edge: 3,
 				center: { x: 0, y: 0 }
@@ -45,7 +65,7 @@ window.onload = function() {
 		var d = (now - delta) / config.hz;
 		delta = now;
 		update(d);
-		render();
+		render(d);
 		requestAnimationFrame(loop);
 	}
 
@@ -55,80 +75,55 @@ window.onload = function() {
 			g.center.y = pointer.y;
 			g.rotation -= config.speed.current;
 		});
-		if (pointer.hold && config.speed.current < config.speed.max) {
-			config.speed.current += config.speed.increment * delta;
+		if (pointer.hold) {
+			config.speed.current = Math.min(config.speed.max, config.speed.current + config.speed.increment * delta);
 		} else if (config.speed.current > 0) {
-			config.speed.current -= config.speed.increment * delta;
+			config.speed.current = Math.max(0, config.speed.current - config.speed.increment * delta);
 		}
+		if (config.isSlowMotion) {
+			config.hz = Math.min(config.minHz, config.hz + 1);
+		} else {
+			config.hz = Math.max(config.maxHz, config.hz - 1);
+		}
+		config.speed.max = Math.PI * 0.12 * delta;
 		particles.forEach(p => p.update(delta));
 		//
 		if (config.speed.current > 0 && pointer.y + config.gearRadius > canvas.height) {
-			if (particles.length < config.maxParticles && Math.random() < delta) {
-				let count = ~~(Math.random() * 3 + 1);
+			if (particles.length < config.maxParticles && Math.random() < config.speed.current * delta / config.speed.max) {
+				let count = ~~(Math.random() * config.perParticles + 1);
 				[...new Array(count)].forEach(() => {
-					particles.push({
-						x: pointer.x + Math.random() * config.gearRadius * 0.25,
+					particles.push(particle({
+						x: pointer.x + Math.random() * config.gearRadius * 0.25 + (config.gearRadius - ~~(canvas.height - pointer.y) * 0.8),
 						y: canvas.height,
-						thickness: Math.random(),
-						velocity: { direction: -Math.random(), length: 60 * Math.max(config.speed.current / config.speed.max, 0.4) },
-						accelarate: { direction: Math.random() * 0.12 - 0.06 },
-						gravity: { direction: Math.PI * 0.5, force: 0.04 + Math.random() * 0.06 },
-						friction: 0.96,
-						lifespan: 70 + Math.random() * 30,
-						update(delta) {
-							this.lifespan -= 1 * delta;
-							this.velocity.length -= (1 - this.friction) * this.velocity.length * delta;
-							this.x += Math.cos(this.velocity.direction) * this.velocity.length * delta;
-							this.y += Math.sin(this.velocity.direction) * this.velocity.length * delta;
-							this.combine(delta);
-							if (this.lifespan < 0) {
-								let index = particles.findIndex(p => p === this);
-								particles.splice(index, 1);
-							}
-						},
-						combine(delta) {
-							let step = this.gravity.force * delta;
-							step += this.accelarate.direction * delta;
-							this.velocity.direction += step;
-						},
-						render(ctx) {
-							ctx.beginPath();
-							ctx.strokeStyle = config.sparkColor;
-							ctx.lineWidth = config.sparkWidth * this.thickness * this.lifespan / 80;
-							ctx.moveTo(this.x, this.y);
-							ctx.lineTo(
-								this.x + Math.cos(this.velocity.direction) * this.velocity.length, 
-								this.y + Math.sin(this.velocity.direction) * this.velocity.length
-							);
-							ctx.stroke();
-							ctx.closePath();
-						}
-					});
+						direction: -Math.random() - 0.1,
+						length: 100
+					}));
 				});
 			}
 		}
 	}
-	
-	function render() {
-		ctx.globalAlpha = 0.8;
+
+	function render(delta) {
+		ctx.globalAlpha = 0.65 * !config.isExposure ? 1 : 0.02;
 		ctx.globalCompositeOperation = 'source-over';
-		ctx.fillStyle = '#000';
+		ctx.fillStyle = config.clearColor;
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 		ctx.globalAlpha = 1;
 
 		// fill gears
+		ctx.globalAlpha = 0.8;
 		ctx.fillStyle = config.gearColor;
 		gears.forEach(g => {
 			ctx.beginPath();
 			let center = g.center;
-			let point = { 
+			let point = {
 				x: Math.cos(g.rotation) * g.radius + center.x,
 				y: Math.sin(g.rotation) * g.radius + center.y
 			}
 			ctx.moveTo(point.x, point.y);
 			[...new Array(g.edge)].forEach((v, i) => {
 				if (i == 0) return;
-				let point = { 
+				let point = {
 					x: Math.cos(g.rotation + i * 2 / 3 * Math.PI) * g.radius + center.x,
 					y: Math.sin(g.rotation + i * 2 / 3 * Math.PI) * g.radius + center.y
 				}
@@ -139,20 +134,105 @@ window.onload = function() {
 		});
 
 		// particles
+		ctx.globalAlpha = 1;
 		ctx.globalCompositeOperation = 'screen';
-		particles.forEach(p => p.render(ctx));
+		particles.forEach(p => p.render(ctx, delta));
 	}
-	
-	window.onmousemove = function(e) {
+
+	function particle(options) {
+		return {
+			x: options.x,
+			y: options.y,
+			thickness: Math.random() + 0.5,
+			velocity: { base: options.base || 0.65, direction: options.direction, length: options.length * Math.max(config.speed.current / config.speed.max, 0.2) },
+			accelarate: { max: 0, min: -Math.PI * 0.5, change: Math.random() * 0.2 - 0.1 },
+			gravity: { direction: Math.PI * 0.5, force: 0.04 + Math.random() * 0.02 },
+			friction: 0.725 + Math.random() * 0.12,
+			lifespan: 40 + Math.random() * 24,
+			first: options.first || 1,
+			update(delta) {
+				this.lifespan -= 1 * delta;
+				this.velocity.length = Math.max(this.velocity.length - (1 - this.friction) * this.velocity.length * delta, 2);
+				let x = this.x + Math.cos(this.velocity.direction) * this.velocity.length * delta;
+				let y = this.y + Math.sin(this.velocity.direction) * this.velocity.length * delta;
+				this.rotation = Math.atan2(y - this.y, x - this.x);
+				this.x = x;
+				this.y = y;
+				this.combine(delta);
+				if (this.lifespan < 0) {
+					let index = particles.findIndex(p => p === this);
+					particles.splice(index, 1);
+				}
+			},
+			combine(delta) {
+				if (this.first == 1) {
+					this.accelarate.max = this.velocity.direction + this.velocity.base;
+					this.accelarate.min = this.velocity.direction - this.velocity.base;
+				}
+				let step = this.accelarate.change * delta;
+				this.velocity.direction += step;
+				if (this.velocity.direction > this.accelarate.max || this.velocity.direction < this.accelarate.min) {
+					this.accelarate.change *= -1;
+				}
+				this.velocity.direction += this.gravity.force * delta;
+				this.first = Math.max(0, this.first - delta);
+				if (Math.random() > 0.996 && this.velocity.length > 10) {
+					let count = ~~(Math.random() * config.perParticles + 2);
+					[...new Array(count)].forEach(() => {
+						particles.push(particle({
+							x: this.x,
+							y: this.y,
+							base: Math.PI,
+							direction: -Math.random() * Math.PI * 2,
+							length: Math.min(this.velocity.length * (Math.random() + 1), 40)
+						}));
+					});
+				}
+			},
+			render(ctx, delta) {
+				let w = config.sparkWidth * this.thickness * this.velocity.length * delta;
+				let h = config.sparkWidth * this.thickness * this.lifespan * 1.25 / 80;
+				w = Math.max(w, h);
+				ctx.translate(this.x, this.y);
+				ctx.rotate(this.rotation);
+				ctx.drawImage(buffer, -w * 0.5, -h * 0.5, w, h);
+				ctx.rotate(-this.rotation);
+				ctx.translate(-this.x, -this.y);
+				if (this.first > 0) {
+					w /= delta * 5;
+					ctx.globalAlpha = Math.random() * 0.8;
+					ctx.drawImage(buffer, this.x - w, this.y - w, w * 2, w * 2);
+					ctx.globalAlpha = 1;
+				}
+			}
+		}
+	}
+
+	window.onmousemove = function (e) {
 		pointer.x = e.clientX;
 		pointer.y = e.clientY;
 	}
 
-	window.onmousedown = function() {
+	window.onmousedown = function () {
 		pointer.hold = true;
 	}
 
-	window.onmouseup = function() {
+	window.onmouseup = function () {
 		pointer.hold = false;
+	}
+
+	window.onkeydown = function (e) {
+		if (e.keyCode == 17) {
+			config.isSlowMotion = true;
+		}
+	}
+
+	window.onkeyup = function (e) {
+		if (e.keyCode == 17) {
+			config.isSlowMotion = false;
+		}
+		if (e.keyCode == 32) {
+			config.isExposure = !config.isExposure;
+		}
 	}
 }
